a8f2ce48820bac271960c3c931f332f7ae748b22
Renan Vianna <renan_eleto_viana@hotmail.com>
2020-07-23 22:28:42 -0300

    Include chute_o_numero.py

---
+++ chute_o_numero.py
@@ -0,0 +1,33 @@
+import random as rd
+
+n = rd.randrange(100)
+print(n)
+j = True
+
+while j:
+    v = int(input('Digite um numero de 1 a 100 ou digite 0 para parar: '))
+    d = ''
+
+    if v == 0:
+        break    
+    elif v == n:
+        print("You win!!!")
+        while True:            
+            d = input('Deseja jogar novamente?(s - sim / n não ')
+            if d == 's':
+                n = rd.randrange(100)
+                break
+            elif d == 'n':
+                j = False
+                break
+    else:
+        t = ('maior' if v > n else 'menor')        
+        print(f'Você errou, o número que digitou é *{t}* que o número aletório. Tente novamente.\n')
+
+
+print('\n###### Obrigado por jogar ######')
+
+    
+
+
+
